9769bc5aecec792d653886a770d4a8b45a8867b3
leilakbari <l_akbari@fanshaweonline.ca>
2023-10-24 01:28:27 -0400

    sass watching

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
-const onProgress = (event) => {
-  const progressBar = event.target.querySelector('.progress-bar');
-  const updatingBar = event.target.querySelector('.update-bar');
-  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
-  if (event.detail.totalProgress === 1) {
-    progressBar.classList.add('hide');
-    event.target.removeEventListener('progress', onProgress);
-  } else {
-    progressBar.classList.remove('hide');
+// const onProgress = (event) => {
+//   const progressBar = event.target.querySelector('.progress-bar');
+//   const updatingBar = event.target.querySelector('.update-bar');
+//   updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+//   if (event.detail.totalProgress === 1) {
+//     progressBar.classList.add('hide');
+//     event.target.removeEventListener('progress', onProgress);
+//   } else {
+//     progressBar.classList.remove('hide');
+//   }
+// };
+// document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+(() => {
+  console.log("IIFE Fired");
+  
+  // variables
+  const model = document.querySelector("#model");
+  const hotSpots = document.querySelectorAll;(".Hotspot");
+
+  // functions
+  function loaded () {
+    console.log(hotSpots);
+
   }
-};
-document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+  // event listeners
+  model.addEventListener("load", loaded);
+  
+})();
